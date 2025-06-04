bd0673c5ec18721df6bcf0a3f8f62356bac6265d
Andrei Roque <roqueandrei0316@gmail.com>
2025-06-04 13:06:54 +0800

    Initialize a window using JFrame

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
+import javax.swing.JFrame;
 
 public class Main{
 
   public static void main(String[] args){
 
-    
+    JFrame window = new JFrame();
+
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setResizable(false);
+    window.setTitle("A * pathfinding algorithm");
+
+    window.setLocationRelativeTo(null);
+    window.setVisible(true);
 
   }
 
